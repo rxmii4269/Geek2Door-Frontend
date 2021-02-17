160adf8add8597a50afca9a41ca0589bfec4767a
Romaine Murray <romaine.murray@mymona.uwi.edu>
2021-02-16 21:25:15 -0500

    set interaction mode to eager

--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,6 +1,7 @@
-import { extend } from 'vee-validate'
+import { extend, setInteractionMode } from 'vee-validate'
 import { required, email } from 'vee-validate/dist/rules'
 
+setInteractionMode('eager')
 extend('required', {
   ...required,
   message: 'This field is required.',

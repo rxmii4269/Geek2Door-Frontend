0e4323c6b47be91af6071a08b1190e429f4f6fa1
Romaine Murray <romaine.murray@mymona.uwi.edu>
2021-03-19 15:55:44 -0500

    implemented company user edit profile

--- pages/users/company/_slug/index.vue
+++ pages/users/company/_slug/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div v-if="!userData.message" class="columns is-8">
+    <div v-if="!profileData.message" class="columns is-8">
       <div class="column is-3">
         <div class="card">
           <div class="card-image">
@@ -13,7 +13,7 @@
           </div>
           <div class="card-content">
             <div
-              v-if="userData.username == $auth.user.username"
+              v-if="profileData.username == $auth.user.username"
               class="test is-clearfix"
             >
               <b-tooltip
@@ -33,58 +33,28 @@
             </div>
             <div class="media mt-5">
               <div class="media-left">
-                <figure v-if="userData" class="image is-48x48">
+                <figure v-if="profileData" class="image is-48x48">
                   <img
-                    :src="`${$axios.defaults.baseURL}/images/${userData.profile_picture}`"
+                    :src="`${$axios.defaults.baseURL}/images/${profileData.profile_picture}`"
                     alt="Profile Picture"
                   />
                 </figure>
               </div>
               <div class="media-content mt-2">
-                <b-field v-if="edit" label="Name" label-position="on-border">
-                  <b-input
-                    v-model.trim.lazy="updatedData.company_name"
-                  ></b-input>
-                </b-field>
-                <p v-else class="title is-4">{{ userData.company_name }}</p>
+                <p class="title is-4">{{ profileData.company_name }}</p>
               </div>
             </div>
-            <b-field v-if="edit" label="Email" label-position="on-border">
-              <b-input v-model.trim.lazy="updatedData.email"></b-input>
-            </b-field>
 
-            <p v-else class="subtitle is-6">{{ userData.email }}</p>
+            <p class="subtitle is-6">{{ profileData.email }}</p>
 
             <div class="content">
-              <b-field
-                v-if="edit"
-                label="Description"
-                label-position="on-border"
-                class="mt-3"
-              >
-                <b-input
-                  v-model.trim.lazy="updatedData.company_desc"
-                  maxlength="200"
-                  type="textarea"
-                ></b-input>
-              </b-field>
-              <p v-else>{{ userData.company_desc }}</p>
+              <p>{{ profileData.company_desc }}</p>
               <b-button
-                v-if="userData.username != $auth.user.username"
+                v-if="profileData.username != $auth.user.username"
                 expanded
                 type="is-pink"
                 @click="messageUser()"
                 >Message</b-button
-              >
-            </div>
-            <div v-if="edit" class="buttons is-centered">
-              <b-button
-                type="is-danger is-outlined card-footer-item"
-                @click="editProfile"
-                >Cancel</b-button
-              >
-              <b-button type="is-primary card-footer-item" @click="saveProfile"
-                >Save</b-button
               >
             </div>
           </div>
@@ -94,12 +64,75 @@
         <div class="box"></div>
       </div>
     </div>
-    <div v-if="userData.message" class="hero is-warning is-medium">
+    <div v-if="profileData.message" class="hero is-warning is-medium">
       <div class="hero-body">
-        <p class="title">{{ userData.message }}</p>
+        <p class="title">{{ profileData.message }}</p>
       </div>
     </div>
     <Chat v-if="$auth.loggedIn" ref="Chat" />
+    <b-modal v-model="edit" has-modal-card>
+      <div class="modal-card">
+        <div class="modal-card-head">
+          <h1>Edit Profile</h1>
+        </div>
+        <div class="modal-card-body">
+          <b-field label="Name" label-position="on-border">
+            <b-input
+              v-model.trim.lazy="updatedProfileData.company_name"
+            ></b-input>
+          </b-field>
+          <b-field label="Email" label-position="on-border">
+            <b-input v-model.trim.lazy="updatedProfileData.email"></b-input>
+          </b-field>
+          <b-field label="Description" label-position="on-border" class="mt-3">
+            <b-input
+              v-model.trim.lazy="updatedProfileData.company_desc"
+              maxlength="200"
+              type="textarea"
+            ></b-input>
+          </b-field>
+          <h1>Address Information</h1>
+          <b-field grouped group-multiline class="mt-3">
+            <p class="control">
+              <b-field label="Street" label-position="on-border">
+                <b-input
+                  v-model.trim.lazy="updatedProfileData.street"
+                  expanded
+                ></b-input>
+              </b-field>
+            </p>
+            <p class="control">
+              <b-field label="Parish" label-position="on-border">
+                <b-input
+                  v-model.trim.lazy="updatedProfileData.parish"
+                  expanded
+                ></b-input>
+              </b-field>
+            </p>
+            <p class="control">
+              <b-field label="City" label-position="on-border">
+                <b-input
+                  v-model.trim.lazy="updatedProfileData.city"
+                  expanded
+                ></b-input>
+              </b-field>
+            </p>
+          </b-field>
+        </div>
+        <div class="modal-card-foot is-centered">
+          <div class="buttons is-centered">
+            <b-button
+              type="is-danger is-outlined card-footer-item"
+              @click="cancel"
+              >Cancel</b-button
+            >
+            <b-button type="is-primary card-footer-item" @click="saveProfile"
+              >Save</b-button
+            >
+          </div>
+        </div>
+      </div>
+    </b-modal>
   </div>
 </template>
 <script>
@@ -107,15 +140,27 @@ import Talk from 'talkjs'
 export default {
   data() {
     return {
-      userData: '',
       chatWith: '',
       edit: false,
-      updatedData: '',
     }
   },
+  computed: {
+    profileData: {
+      get() {
+        return this.$store.state.profileData
+      },
+    },
+    updatedProfileData: {
+      get() {
+        return this.$store.state.updatedProfileData
+      },
+      set(value) {
+        this.$store.commit('', value)
+      },
+    },
+  },
   async mounted() {
-    const user = await this.$axios.$get(`/users/${this.$route.params.slug}`)
-    this.userData = user
+    await this.$store.dispatch('getProfile', this.$route.params.slug)
   },
   methods: {
     async messageUser() {
@@ -127,9 +172,9 @@ export default {
           role: 'buyer',
         })
         const other = new Talk.User({
-          id: this.userData.id,
-          name: this.userData.fullname,
-          email: this.userData.email,
+          id: this.profileData.id,
+          name: this.profileData.fullname,
+          email: this.profileData.email,
           role: 'seller',
         })
 
@@ -154,18 +199,15 @@ export default {
     },
     editProfile() {
       this.edit = !this.edit
-      this.updatedData = this.userData
+      this.$store.dispatch('editProfile', this.profileData)
     },
-    async saveProfile() {
-      const self = this
-      await this.$axios
-        .$post(`/users/company/${this.$auth.user.id}`, this.updatedData)
-        .then(async function (response) {
-          const updatedUser = await self.$axios.$get('/auth/user')
-          await self.$auth.setUser(updatedUser.user)
-          self.$router.push(`/users/company/${self.$auth.user.name}`)
-          self.$store.dispatch('generateProfileUrl')
-        })
+    cancel() {
+      this.$store.dispatch('getProfile', this.$route.params.slug)
+      this.edit = !this.edit
+    },
+    saveProfile() {
+      this.$store.dispatch('saveProfile', this.updatedProfileData)
+      this.edit = !this.edit
     },
   },
 }
--- store/index.js
+++ store/index.js
@@ -1,10 +1,18 @@
 export const state = () => ({
   profile_url: '',
+  profileData: '',
+  updatedProfileData: '',
 })
 
 export const mutations = {
   SET_PROFILE_URL(state, url) {
     state.profile_url = url
+  },
+  SET_PROFILE_DATA(state, profileData) {
+    state.profileData = profileData
+  },
+  SET_UPDATED_PROFILE_DATA(state, profileData) {
+    state.updatedProfileData = profileData
   },
 }
 
@@ -19,5 +27,29 @@ export const actions = {
       }
       commit('SET_PROFILE_URL', url)
     }
+  },
+  editProfile({ commit }, profile) {
+    commit('SET_UPDATED_PROFILE_DATA', profile)
+  },
+  async getProfile({ commit }, userIdentifier) {
+    const response = await this.$axios.$get(`/users/${userIdentifier}`)
+    commit('SET_PROFILE_DATA', response)
+  },
+  async saveProfile({ commit, dispatch }, profile) {
+    const self = this
+    await this.$axios
+      .$post(`/users/company/${this.$auth.user.id}`, profile)
+      .then(async function (response) {
+        const updatedUser = await self.$axios.$get('/auth/user')
+        await self.$auth.setUser(updatedUser.user)
+        if (self.$auth.user.role === 'company') {
+          self.$router.push(`/users/company/${self.$auth.user.name}`)
+          dispatch('getProfile', updatedUser.user.name)
+        } else {
+          dispatch('generateProfileUrl')
+          self.$router.push(`/user/student/${self.$auth.user.username}`)
+          dispatch('getProfile', updatedUser.user.username)
+        }
+      })
   },
 }

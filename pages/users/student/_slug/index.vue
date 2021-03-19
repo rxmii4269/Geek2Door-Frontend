357c0ff92e86b9204bf2d08b5d75423b5b40db22
Danielle <dannziiwan@gmail.com>
2021-03-19 11:33:52 -0500

    create the blog view

--- pages/users/student/_slug/index.vue
+++ pages/users/student/_slug/index.vue
@@ -46,6 +46,42 @@
       </div>
       <div class="column is-9">
         <div class="box"></div>
+        <b-button type="is-primary mt-6">Create Blog </b-button>
+        <div class="card column is-4 mt-4">
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <img
+                src="https://bulma.io/images/placeholders/1280x960.png"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <div class="card-content">
+            <div class="media">
+              <div class="media-content">
+                <p class="title is-4">Some Blog Title</p>
+                <p class="subtitle is-6">by: {{ userData.username }}</p>
+              </div>
+            </div>
+            <div class="content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+              nec iaculis mauris. <a>@thingsStressingYou</a>.
+              <a href="#">#LifeonlyGetsWorse</a>
+              <a href="#">#responsive</a>
+              <br />
+
+              <b-button slim type="is-pink" class="is-pulled-left"
+                >Comment</b-button
+              >
+              <b-button slim type="is-pink" class="is-pulled-left"
+                >Edit</b-button
+              >
+              <br />
+              <br />
+              <time datetime="2016-1-1">11:09 PM - 1 Jan 2021</time>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div v-if="userData.message" class="hero is-warning is-medium">

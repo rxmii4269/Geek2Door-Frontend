22aace2c1344e8f42750a0686745ca3bd21f0941
Marc Grant <marcgrant@pop-os.localdomain>
2021-04-06 00:53:08 -0500

    Homepage for users

---
+++ components/CompanyHomePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="columns is-8">
+    <div class="column is-3">
+      <div class="card">
+        <div class="card-content">
+          <div class="content">
+            <h5>Hello These are your recommended Students:</h5>
+          </div>
+        </div>
+      </div>
+      <b-button @click="$auth.refreshTokens()">Refresh</b-button>
+    </div>
+    <div class="column is-9">
+      <div class="container mb-5">
+        <b-field expanded>
+          <b-input
+            expanded
+            rounded
+            icon-pack="bx"
+            icon-right="bx-search"
+            placeholder="Search"
+          ></b-input>
+        </b-field>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState(['allStudents']),
+  },
+  async mounted() {
+    await this.$store.dispatch('getAllStudents')
+  },
+}
+</script>

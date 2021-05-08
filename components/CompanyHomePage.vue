27147963d169cbfe090176c6310ba4d4856020bd
Romaine Murray <romaine.murray@mymona.uwi.edu>
2021-05-07 19:15:31 -0500

    fixed regression for homepage

--- components/CompanyHomePage.vue
+++ components/CompanyHomePage.vue
@@ -93,7 +93,7 @@ export default {
     if (process.browser) {
       // eslint-disable-next-line nuxt/no-globals-in-created
       window.addEventListener('load', () => {
-        this.$store.dispatch('getAllInternships')
+        this.$store.dispatch('getAllStudents')
       })
     }
   },

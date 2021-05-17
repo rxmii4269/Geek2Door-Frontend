ea3d94294f98842866e7fb4e0ab2c87cbca926af
Romaine Murray <romaine.murray@mymona.uwi.edu>
2021-05-17 00:23:02 -0500

    started breadcrumbs

--- layouts/default.vue
+++ layouts/default.vue
@@ -15,6 +15,28 @@ export default {
       isForm: false,
     }
   },
+  computed: {
+    crumbs() {
+      const fullPath = this.$route.fullPath
+      const params = fullPath.substring(1).split('/')
+      const crumbs = []
+      let path = ''
+
+      params.forEach((param, index) => {
+        path = `${path}/${param}`
+        const match = this.$router.match(path)
+        if (match.name !== null) {
+          crumbs.push({ title: param.replace(/-/g, ' '), ...match })
+          console.log(`yep: ${path}`)
+        } else {
+          console.log(`nope: ${path}`)
+        }
+        console.log(path)
+      })
+      console.log(params)
+      return crumbs
+    },
+  },
   watch: {
     $route() {
       if (this.$route.name.includes('accounts')) {
